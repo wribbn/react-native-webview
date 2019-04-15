e1e12b0046fef8370ba3a65945b06061cbfda2e3
paulxuca <paulxuca@gmail.com>
2019-04-15 10:55:16 -0400

    Fix ts type

--- index.d.ts
+++ index.d.ts
@@ -5,36 +5,42 @@ import { IOSWebViewProps, AndroidWebViewProps } from './lib/WebViewTypes';
 export type WebViewProps = IOSWebViewProps & AndroidWebViewProps;
 
 declare class WebView extends Component<WebViewProps> {
-    /**
-     * Go back one page in the webview's history.
-     */
-    goBack: () => void;
+  /**
+   * Go back one page in the webview's history.
+   */
+  goBack: () => void;
 
-    /**
-     * Go forward one page in the webview's history.
-     */
-    goForward: () => void;
+  /**
+   * Go forward one page in the webview's history.
+   */
+  goForward: () => void;
 
-    /**
-     * Reloads the current page.
-     */
-    reload: () => void;
+  /**
+   * Reloads the current page.
+   */
+  reload: () => void;
 
-    /**
-     * Stop loading the current page.
-     */
-    stopLoading(): void;
+  /**
+   * Stop loading the current page.
+   */
+  stopLoading(): void;
 
-    /**
-     * Extra Native Component Config.
-     */
-    extraNativeComponentConfig: () => any;
+  /**
+   * Extra Native Component Config.
+   */
+  // eslint-disable-next-line
+  extraNativeComponentConfig: () => any;
 
-    /**
-     * Executes the JavaScript string.
-     */
-    injectJavaScript: (script: string) => void;
+  /**
+   * Executes the JavaScript string.
+   */
+  injectJavaScript: (script: string) => void;
+
+  /**
+   * Sends a messsage to the webview
+   */
+  postMessage: (message: string) => void;
 }
 
-export {WebView};
+export { WebView };
 export default WebView;
